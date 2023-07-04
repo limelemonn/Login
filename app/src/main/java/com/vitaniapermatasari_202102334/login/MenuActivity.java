553ca7fe24cef5768b5f14b07af7a574355286d0
limelemonn <permatamiavita@gamil.com>
2023-07-04 11:38:49 +0700

    Login

---
+++ app/src/main/java/com/vitaniapermatasari_202102334/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.vitaniapermatasari_202102334.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
